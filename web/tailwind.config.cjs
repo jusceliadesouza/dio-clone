038d408ad4ff6f950ecd7bda62827dbc4c14e1c0
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-12-03 00:20:36 -0300

    configurações iniciais

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          400: "#23DD7A",
+        },
+        purple: {
+          900: "#1E192C",
+          700: "#8647AD",
+        },
+        pink: {
+          700: "#E4105D",
+        },
+        yellow: {
+          300: "#E5E044",
+        },
+        zinc: {
+          900: "#151515",
+          600: "#565656",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
